a5adcb63255983a69266d47899dbf9eef7f7b042
Sergey Goncharuk <fleshed914@gmail.com>
2021-08-30 19:47:31 +0300

    feat: add properties to TicTacToe class, add nextTurn method

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,14 +1,21 @@
 class TicTacToe {
-    constructor() {
-
-    }
+    currentPlayer = 'x';
+    gameField = [
+        [ null, null, null ],
+        [ null, null, null ],
+        [ null, null, null ]
+    ]
 
     getCurrentPlayerSymbol() {
-
+        return this.currentPlayer;
     }
 
     nextTurn(rowIndex, columnIndex) {
-
+        if(this.gameField[rowIndex][columnIndex]) {
+            return;
+        }
+        this.gameField[rowIndex][columnIndex] = this.currentPlayer;
+        this.currentPlayer = this.currentPlayer == 'x' ? 'o' : 'x';
     }
 
     isFinished() {
@@ -28,7 +35,7 @@ class TicTacToe {
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+        return this.gameField[rowIndex][columnIndex];
     }
 }
 
